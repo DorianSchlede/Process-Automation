562485376785a1d028b81d637f3757936d72a03d
DorianSchlede <80420885+DorianSchlede@users.noreply.github.com>
2023-09-06 22:11:09 +0200

    x

--- generate.py
+++ generate.py
@@ -9,6 +9,12 @@ os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 
 delimiter = "###"
 
+# Streamlit UI
+st.title("Process CSV Generator")
+st.write("Use this tool to generate any process in detail. Just type in the name and your situation and get a CSV file of processes, inputs and outputs. The generation will take about 3 Minutes.")
+st.image('https://i.imgur.com/kNfSHLC.png', caption='Example Output.', use_column_width=True)
+process_name = st.text_input("Enter the process name:", "User Research")
+company_type = st.text_input("Enter the company type:", "Startup")
 
 def call_openai_api(prompt, max_tokens=100, model="gpt-4", temperature=0):
     try:
@@ -27,15 +33,10 @@ def call_openai_api(prompt, max_tokens=100, model="gpt-4", temperature=0):
 
 
 def generate_expert_role(process_name, company_type):
-    try:
-        prompt = f"Describe an expert role with knowledge about {process_name} for {company_type} in up to 40 words. Write in plain text. Start your reply with 'You are'."
-        expert_role = call_openai_api(prompt, max_tokens=100, model="gpt-3.5-turbo")
-        st.write(f"Generated expert role: {expert_role}")
-        return expert_role
-    except TypeError as te:
-        st.write(f"TypeError: {str(te)}")
-    except Exception as e:
-        st.write(f"An unexpected error occurred: {str(e)}")
+    prompt = f"Describe an expert role with knowledge about {process_name} for {company_type} in up to 40 words. Write in plain text. Start your reply with 'You are'."
+    expert_role = call_openai_api(prompt, max_tokens=100, model="gpt-3.5-turbo")
+    print(f"Generated expert role: {expert_role}")
+    return expert_role
 
 def generate_phases(process_name, expert_role, company_type):
     prompt = f"{expert_role}. Generate a process for {process_name}. It is made for {company_type}. Map out the 3-5 high level phases of this process. Define Input, Task and Output for each of them in a few bullet points."
@@ -65,6 +66,7 @@ def generate_steps(process_name, expert_role, phases, company_type):
     print(f"Steps: {steps}")
     return steps
 
+
 def generate_library(steps):
     prompt=f"""You will take the content {steps} and output is as a python dictionary.
          The dictionary will be formatted the following way {{
@@ -84,17 +86,15 @@ def generate_library(steps):
     print(f"Library: {library}")
     return library
 
-def generate_csv_string(dictionary):
-    output = io.StringIO()
-    fieldnames = ['ID', 'Process Level 1', 'Process Level 2', 'Process Level 3', 'Input', 'Task', 'Output']
-    writer = csv.DictWriter(output, fieldnames=fieldnames)
-    writer.writeheader()
-    for id, value in dictionary.items():
-        row = {'ID': id}
-        row.update(value)
-        writer.writerow(row)
-    return output.getvalue()
-
+def write_dict_to_csv(dictionary, filename):
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['ID', 'Process Level 1', 'Process Level 2', 'Process Level 3', 'Input', 'Task', 'Output']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for id, value in dictionary.items():
+            row = {'ID': id}
+            row.update(value)
+            writer.writerow(row)
 
 def str_to_dict(dict_str):
     """Converts a dictionary string to a dictionary object."""
@@ -103,97 +103,57 @@ def str_to_dict(dict_str):
     except (SyntaxError, ValueError):
         return "Conversion failed"
 
+# Initialize the session state variable if it doesn't exist yet
+if 'library_dict' not in st.session_state:
+    st.session_state.library_dict = None
 
-# Streamlit UI
-st.title("Process CSV Generator")
-
-# Create session state variable for page if it doesn't exist
-if 'page' not in st.session_state:
-    st.session_state.page = "Generate Process"
-
-# Sidebar for selection
-st.sidebar.markdown("**Menu**")
-if st.sidebar.button("Generate Process"):
-    st.session_state.page = "Generate Process"
-if st.sidebar.button("All Processes"):
-    st.session_state.page = "All Processes"
-
-if st.session_state.page == "Generate Process":
-    st.write("Use this tool to generate any process in detail. Just type in the name and your situation and get a CSV file of processes, inputs and outputs. The generation will take about 3 Minutes.")
-    st.image('https://i.imgur.com/kNfSHLC.png', caption='Example Output.', use_column_width=True)
-    process_name = st.text_input("Enter the process name:", "User Research")
-    company_type = st.text_input("Enter the company type:", "Startup")
-
-    if st.session_state.library_dict is not None:
-        if st.session_state.library_dict != "Conversion failed":
-            csv_string = generate_csv_string(st.session_state.library_dict)  # Using the new function here
-            df = pd.read_csv(io.StringIO(csv_string))  # Reading the CSV string to DataFrame
-
-            st.subheader('Generated CSV Data')
-            st.dataframe(df)
-
-            st.download_button(
-                label="Download Library CSV",
-                data=csv_string,
-                file_name=f"{process_name}_library.csv",
-                mime="text/csv"
-            )
-
-
-    # Button to trigger all parts in sequence
-    if st.button("Generate CSV File"):
+# Button to trigger all parts in sequence
+if st.button("Generate CSV File"):
     
-        # Show a spinner while generating the expert role
-        with st.spinner('Generating Expert Role...'):
-            expert_role = generate_expert_role(process_name, company_type)
-        st.success('Expert Role Generated!')
-        st.subheader('CSV Generation Process')
-        st.write("Generated Expert Role: " + expert_role + "\n" + "---")
+    # Show a spinner while generating the expert role
+    with st.spinner('Generating Expert Role...'):
+        expert_role = generate_expert_role(process_name)
+    st.success('Expert Role Generated!')
+    st.subheader('CSV Generation Process')
+    st.write("Generated Expert Role: " + expert_role + "\n" + "---")
 
-        # Show a spinner while generating phases
-        with st.spinner('Generating Phases...'):
-            phases = generate_phases(process_name, expert_role, company_type)
-        st.success('Phases Generated!')
-        st.write(f"Generated Phases: {phases}  \n---")
+    # Show a spinner while generating phases
+    with st.spinner('Generating Phases...'):
+        phases = generate_phases(process_name, expert_role)
+    st.success('Phases Generated!')
+    st.write(f"Generated Phases: {phases}  \n---")
 
+    
+    # Show a spinner while generating steps
+    with st.spinner('Generating Steps...'):
+        steps = generate_steps(process_name, expert_role, phases, company_type)
+    st.success('Steps Generated!')
+    st.write(f"Generated Steps: {steps}  \n---")
+
+    # Show a spinner while generating library
+    with st.spinner('Generating Library...'):
+        library = generate_library(steps)
+    st.session_state.library_dict = str_to_dict(library)  # Store in session state
+    
+  # Check if there's data in the session state to display
+library_dict = st.session_state.get('library_dict', None)
+
+# Check if there's data in the session state to display
+if library_dict is not None:
+    # your existing code
+    if st.session_state.library_dict != "Conversion failed":
+        csv_file_name = f"{process_name}_generated.csv"
+        write_dict_to_csv(st.session_state.library_dict, csv_file_name)
         
-        # Show a spinner while generating steps
-        with st.spinner('Generating Steps...'):
-            steps = generate_steps(process_name, expert_role, phases, company_type)
-        st.success('Steps Generated!')
-        st.write(f"Generated Steps: {steps}  \n---")
-
-        # Show a spinner while generating library
-        with st.spinner('Generating Library...'):
-            library = generate_library(steps)
-        st.session_state.library_dict = str_to_dict(library)  # Store in session state
+        df = pd.read_csv(csv_file_name)
+        st.subheader('Generated CSV Data')
+        st.dataframe(df)
         
-    if st.session_state.library_dict is not None:
-        if st.session_state.library_dict != "Conversion failed":
-            csv_string = generate_csv_string(st.session_state.library_dict)
-            df = pd.read_csv(io.StringIO(csv_string))
-
-            st.subheader('Generated CSV Data')
-            st.dataframe(df)
-
-            st.download_button(
-                label="Download Library CSV",
-                data=csv_string,
-                file_name=f"{process_name}_library.csv",
-                mime="text/csv"
-            )
-        else:
-            st.error("Failed to convert library string to dictionary.")
-
-
-
-
-elif st.session_state.page == "All Processes":
-    st.write("Debug: Entered All Processes block")  # Debugging line
-    # Create directory if it does not exist
-    if not os.path.exists('saved_csvs'):
-        os.makedirs('saved_csvs')
-    saved_csvs = os.listdir('saved_csvs')
-    st.write("List of all saved processes:")
-    for csv in saved_csvs:
-        st.write(csv)
+        st.download_button(
+            label="Download Library CSV",
+            data=pd.read_csv(csv_file_name).to_csv(index=False),
+            file_name="library.csv",
+            mime="text/csv"
+        )
+    else:
+        st.error("Failed to convert library string to dictionary.")
